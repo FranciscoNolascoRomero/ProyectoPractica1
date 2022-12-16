1a17f01265ee979b1d06899844c1ef92c9fce232
FNR <francisnolascoromero@gmail.com>
2022-12-16 13:26:47 +0100

    version 0.0.1 añadido metodo espar

---
+++ Matematicas.java
@@ -0,0 +1,11 @@
+package Arrysbidirreccionales;
+
+public class Matematicas {
+    public static void espar(int numero) {
+        if (numero % 2==0){
+            System.out.printf("ES PAR");
+        }else{
+            System.out.printf("ES IMPAR");
+        }
+    }
+}
